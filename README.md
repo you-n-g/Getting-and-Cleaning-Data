36df245216e6278bffc7178ddfe653ce19bd9428
Young <afe.young@gmail.com>
2015-10-23 09:46:32 +0800

    coursera project

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Getting-and-Cleaning-Data
-Peer Assessments 
+# Getting and Cleaning Data Course Project
+
+This is a repository for the course project of the online course Getting and Cleaning Data from Coursera.
+
+This project contains one R script, `run_analysis.R`, which will calculate means per activity, per subject of the mean and Standard deviation of the Human Activity Recognition Using Smartphones Dataset Version 1.0. The data discription can be found [here](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) This dataset should be [downloaded](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip ) and extracted directly into the project directory.
+
+Once executed, the resulting dataset can be found at `./TidyData.txt`
+
+For futher details, refer to [CookBook.md](CookBook.md)
+
+
+
+## Required R Packages
+
+The R package `reshape2` is required to run this script. This maybe installed with,
+
+```{r}
+install.package("tidyr")
+install.package("dplyr")
+```
---
+++ run_analysis.R
@@ -0,0 +1,43 @@
+library(tidyr)
+library(dplyr)
+
+# 0. read all the data
+X_train<-read.table("./UCI HAR Dataset/train/X_train.txt")
+X_test<-read.table("./UCI HAR Dataset/test/X_test.txt")
+y_train<-read.table("./UCI HAR Dataset/train/y_train.txt")
+y_test<-read.table("./UCI HAR Dataset/test/y_test.txt")
+sub_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+sub_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+activity_lables<-read.table("./UCI HAR Dataset/activity_labels.txt")
+features<-read.table("./UCI HAR Dataset/features.txt")
+
+
+# 1. Merges the training and the test sets to create one data set.
+X_all <- bind_rows(X_train, X_test)
+y_all <- bind_rows(y_train, y_test)
+sub_all <- bind_rows(sub_train, sub_test)
+
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+valid_column_names <- make.names(names=features[,2], unique=TRUE, allow_ = TRUE) # some name contains "()", that are invalid name to use select
+original_names <- as.character(features[,2])
+names(original_names) <- valid_column_names  # remember the original names.
+
+colnames(X_all) <- valid_column_names
+X_all <- select(X_all, matches("std|mean", ignore.case=FALSE))
+
+# 3. Uses descriptive activity names to name the activities in the data set
+colnames(y_all) <- "activity"
+colnames(sub_all) <-"subject"
+
+# 4. Appropriately labels the data set with descriptive variable names. 
+y_all[,"activity"] =  activity_lables[y_all[["activity"]], 2]
+
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+all_data <- bind_cols(sub_all, y_all, X_all)
+by_activity_subject <- group_by(all_data, activity, subject)
+tidy_data <- summarize_each(by_activity_subject, funs(mean))
+colnames(tidy_data)[-c(1,2)] <- original_names[colnames(tidy_data)[-c(1,2)]]
+write.table(tidy_data, "TidyData.txt")
